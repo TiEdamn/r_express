562041b57d92e1b7e1d2d45be4c79ede857d6847
Igor Tikhanovich <tikhanovich.89@gmail.com>
2019-06-05 00:17:54 +0300

    test project

---
+++ api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Person = require('./person');
+
+router.get('/persons', (req, res) => {
+    Person.find({})
+        .then(persons => res.send(persons))
+});
+
+router.post('/persons', (req, res) => {
+    Person.create(req.body)
+        .then(person => res.send(person));
+});
+
+router.put('/persons/:id', (req, res) => {
+    Person.findByIdAndUpdate({_id: req.params.id}, req.body)
+        .then(() => {
+            Person.findOne({_id: req.params.id})
+                .then(person => res.send(person))
+        })
+});
+
+router.delete('/persons/:id', (req, res) => {
+    Person.deleteOne({_id: req.params.id})
+        .then(person => res.send(person));
+});
+
+module.exports = router;
